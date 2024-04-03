af63677242fdab02796c9744560fb7d9745c5651
dajana-lab <75635513+dajana-lab@users.noreply.github.com>
2024-04-03 22:53:21 +0200

    Improved the users experience

--- src/app.js
+++ src/app.js
@@ -15,8 +15,12 @@ function poemForm(event) {
   let apiKey = "7adbfd2oe230496b6bf0deac7aetb83f";
   let prompt = `Generate a Poem about ${clientInput.value}`;
   let context =
-    "You are a excellent Poem expert and you like to write a short poem, with nice and polite words. Make sure to generate a 5 line poem in HTML and follow the client poem. Make sure to separate each line with a <br />. Do NOT make a huge line height, just clear and simple. Everytime add the title for every Poem";
+    "You are a excellent Poem expert and you like to write a short poem, with nice and polite words. Make sure to generate a 5 line poem in HTML and follow the client poem. Make sure to separate each line with a <br />. Do NOT make a huge line height, just clear and simple. Everytime add the title of Poem";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Generate a Poem about ${clientInput.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
